6d4b1be698dad18db78739e9f6c450280254ad78
Alex <coonax@gmail.com>
2018-07-26 16:26:58 +0200

    web testing

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const bodyParser = require("body-parser");
 const sqlDbFactory = require("knex");
 const process = require("process");
 
-
-
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 const _ = require("lodash");
+
+
+const options = {
+  uri: `https://oroscopo.sky.it/oroscopo/giorno/leone.html`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+
+rp(options)
+  .then(($) => {
+    console.log($('.c-multi-tab__tab-body.j-tabs-tab0.is-active').text());
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 let serverPort = process.env.PORT || 5000;
 
@@ -17,7 +35,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // /* Register REST entry point */
 
+app.get("/oroscopoOdierno/:params", function(req, res) {
+  console.log(req.query.name);
+  console.log(Object.keys(req.query)[0]);
 
+});
 
 // app.use(function(req, res) {
 //   res.status(400);
