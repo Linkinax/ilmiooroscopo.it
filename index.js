49954b0a7dcb5b409138f11ffccad224348c37b5
Alex <coonax@gmail.com>
2018-07-26 14:22:48 +0200

    Things

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const sqlDbFactory = require("knex");
+const process = require("process");
+
+
+
+const _ = require("lodash");
+
+let serverPort = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + "/startbootstrap-clean-blog-gh-pages"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// /* Register REST entry point */
+
+
+
+// app.use(function(req, res) {
+//   res.status(400);
+//   res.send({ error: "400", title: "404: File Not Found" });
+// });
+
+app.set("port", serverPort);
+
+
+/* Start the server on port 3000 */
+app.listen(serverPort, function() {
+  console.log(`Your app is ready at port ${serverPort}`);
+});
