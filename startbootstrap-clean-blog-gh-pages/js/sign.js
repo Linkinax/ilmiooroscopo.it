f45c67ff5a0225fa92a6ec4c51b3ab94dbc3bade
Alex <coonax@gmail.com>
2018-07-27 16:57:57 +0200

    Versione semi-funzionante

--- startbootstrap-clean-blog-gh-pages/js/sign.js
+++ startbootstrap-clean-blog-gh-pages/js/sign.js
@@ -1,43 +1,16 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
-};
+function getSignFromUrl() {
 
-function addServiziOfferti(data, id_res){
-  $.getJSON("/service_single_page/search?ID="+ id_res , function(result){
-         $.each(result, function(i, field){
-             $("#ServiziOfferti").text(field.Servizio);
-             $("#ServiziOfferti")[0].href= "/pages/service_single_page.html?id="+ id_res;
-           });
- });
+  return window.location.search.split("=")[1];
 
- }
-
-function addPersonale(data)
-{
-    var personale = data.Personale.split(",");
-    for(var i=0;i<personale.length;i++)
-        {
-            console.log(personale[i].toString().replace("{","").replace("}",""));
-            $("#PersonaleInfoTab").append("<li>"+personale[i].toString().replace("{","").replace("}","").replace("\"" ,"").replace("\"","")+"</li>");
-        }
 }
-
 
 function testing () {
     $(document).ready(function LoadInfo() {
-         $.getJSON(" /oroscopoOdierno/leone " , function(result){
+         $.getJSON(" /oroscopoOdierno/"+getSignFromUrl() , function(result){
                 $.each(result, function(i, field){
                   console.log(field);
+                    $("#Segno").text("Segno selezionato: "+result.Segno);
                     $("#Titolo").text(("L'oroscopo per: "+result.Segno));
                     $("#DescrizioneGenerale").text((result.Generale));
                     $("#DescrizioneAmore").text((result.Amore));
