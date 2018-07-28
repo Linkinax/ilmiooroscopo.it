baba4e1be45ca3c7149fdd38b96010091beb0de9
Alex <coonax@gmail.com>
2018-07-28 14:45:27 +0200

    Oroscopo settimanale funzionante

--- startbootstrap-clean-blog-gh-pages/js/sign.js
+++ startbootstrap-clean-blog-gh-pages/js/sign.js
@@ -10,7 +10,9 @@ function testing () {
          $.getJSON(" /oroscopoOdierno/"+getSignFromUrl() , function(result){
                 $.each(result, function(i, field){
                   console.log(field);
+
                     $("#Segno").text("Segno selezionato: "+result.Segno);
+                    $("#Data").text("Data: "+result.Data);
                     $("#Titolo").text(("L'oroscopo per: "+result.Segno));
                     $("#DescrizioneGenerale").text((result.Generale));
                     $("#DescrizioneAmore").text((result.Amore));
--- startbootstrap-clean-blog-gh-pages/js/signSettimanale.js
+++ startbootstrap-clean-blog-gh-pages/js/signSettimanale.js
@@ -11,6 +11,7 @@ function testing () {
                 $.each(result, function(i, field){
                   console.log(field);
                     $("#Segno").text("Segno selezionato: "+result.Segno);
+                    $("#Data").text("Data: "+result.Data);
                     $("#Titolo").text(("L'oroscopo per: "+result.Segno));
                     $("#DescrizioneGenerale").text((result.Generale));
                     $("#DescrizioneAmore").text((result.Amore));
