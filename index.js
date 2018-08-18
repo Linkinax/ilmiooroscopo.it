f829848af10567944bf40a5fa1de2eaad28da91d
Alex <coonax@gmail.com>
2018-08-19 00:22:09 +0200

    Worka?! #code dublino

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ function PostaInstagram(Caption, i)
                     console.log(challenge);
                     const challengeUrl = errorObj.checkpoint_url.toString();
                     await client.updateChallenge({ challengeUrl, choice: 0 });
-                    await client.updateChallenge({ challengeUrl, securityCode: 950276  })
+                    await client.updateChallenge({ challengeUrl, securityCode: 678245  })
                     .then( ()=>{console.log("Dovrei Aver finito ")})
                     .catch((err) => {console.log(err)})
                   }
