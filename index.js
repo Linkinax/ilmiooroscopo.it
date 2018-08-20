794b79ac6aa62b3401474236735b1945a85c0cee
Alex <coonax@gmail.com>
2018-08-20 21:14:15 +0200

    new ig num

--- index.js
+++ index.js
@@ -392,8 +392,8 @@ function PostaInstagram(Caption, i)
                     console.log("END of the challenge\n\n");
                     const challengeUrl = errorObj.checkpoint_url.toString();
                     await client.updateChallenge({ challengeUrl, choice: 0 });
-                    await client.updateChallenge({ challengeUrl, securityCode: 570942  })
-                    await client.updateChallenge({ challengeUrl, securityCode: 570942  })
+                    await client.updateChallenge({ challengeUrl, securityCode: 036287  })
+                    await client.updateChallenge({ challengeUrl, securityCode: 036287  })
                     .catch((err) => {console.log(err)})
                   }
                 }})();
